d37a2bb6a69b7d73c4589afc8b58d5b1f4c5beb9
Anatolii Ivanov <gotnickname@gmail.com>
2023-04-16 15:48:23 +0530

    added all catch blocks

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,8 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to exeptions lottery, press any key to start!");
-            Console.ReadKey();
+            try
+            {
+                Exception[] exceptions = {new DivideByZeroException(), new FormatException(),
+                    new OutOfMemoryException(), new StackOverflowException(), new WinningException("You Win!")};
+
+                Console.WriteLine("Welcome to exceptions lottery, press any key to start!");
+                Console.ReadKey();
+                var Randomiser = new Random();  
+                throw exceptions[Randomiser.Next(0, 6)];
+                // Index out of range exception is an easter egg.
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine($"You were close. {ex.Message}");
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine($"Try better next time. {ex.Message}");
+            }
+            catch(OutOfMemoryException ex)
+            {
+                Console.WriteLine($"You tried. {ex.Message}");
+            }
+            catch(StackOverflowException ex)
+            {
+                Console.WriteLine($"This one is cool, but not what you need. {ex.Message}");
+            }
+            catch(WinningException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Thanks for playing! Press any key to exit.");
+                Console.ReadKey();
+            }
         }
+
+    }
+
+    public class WinningException : Exception
+    {
+        public WinningException(string message) : base(message) { }
     }
 }

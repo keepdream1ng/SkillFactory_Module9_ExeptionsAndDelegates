c93a875f77fd74152fe3b1eb5a1267e51d621d44
Anatolii Ivanov <gotnickname@gmail.com>
2023-04-17 14:09:11 +0530

    added start for Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task2
+{
+    internal class StringsSortApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This application will sort the provided strings, or use a demo list to sort, if you didnt one any arguments.");
+            if (args.Length == 0 )
+            {
+                args = new string[] { "Ivanov", "Petrov", "Sidorov", "Semenov", "Kuznetsov" };
+                Console.WriteLine("Here is your demo list:");
+                Console.WriteLine(String.Join(Environment.NewLine, args));  
+            }
+        }
+
+
+    }
+
+    public class InvalidInputException : Exception
+    {
+        public InvalidInputException(string message) : base(message) { }
+    }
+}
